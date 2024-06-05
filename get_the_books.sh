b977bbfcc0203cd804c960cfdba95c30a69a68c0
Ubuntu <ubuntu@ip-172-31-17-231.us-east-2.compute.internal>
2024-06-05 03:00:29 +0000

    bash loop complete

--- get_the_books.sh
+++ get_the_books.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-book_ids=(32037 932 1064 10947 1063 51060)
+
+book_ids=(32037 932 1064 10947 1063 51060 2147 2151 2150 2148)
+
 for id in "${book_ids[@]}"
 do
     wget "https://www.gutenberg.org/cache/epub/$id/pg$id.txt" -O "book_$id.txt"
